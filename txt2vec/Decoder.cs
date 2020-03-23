3188ef9cec472bf3c701f420615326c6ce2f8873
Hao Liu <Hao.Liu@nuance.com>
2020-03-23 15:58:58 +0800

    refreshed for netcore 3.1

--- txt2vec/Decoder.cs
+++ txt2vec/Decoder.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+//using System.Linq;
+//using System.Text;
 using System.Threading.Tasks;
-using System.IO;
-using AdvUtils;
+//using System.IO;
+//using AdvUtils;
 
 namespace Txt2Vec
 {
--- txt2vec/Encoder.cs
+++ txt2vec/Encoder.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+//using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+//using System.Threading.Tasks;
 using System.IO;
 using System.Threading;
 using AdvUtils;
